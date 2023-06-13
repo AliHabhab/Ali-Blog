820d8ceba2e10439b0e874d553c9b093f7a98a60
AliHabhab <alii.habhab01@gmail.com>
2023-06-13 21:09:17 +0300

    addingAllPosts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,42 @@
-const AllPostsPage = () => {};
+import AllPosts from "../../components/posts/all-posts";
+
+const DUUMY_POST = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
+    date: "2023-06-13",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
+    date: "2023-06-13",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
+    date: "2023-06-13",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR",
+    date: "2023-06-13",
+  },
+];
+
+const AllPostsPage = () => {
+  return <AllPosts posts={DUUMY_POST} />;
+};
 
 export default AllPostsPage;
